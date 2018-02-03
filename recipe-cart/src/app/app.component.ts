40352ccd32d86e4ee46c895fec1a5893bdc89674
Jyasveer Gotta <jyasveer@gmail.com>
2018-02-03 13:09:00 +0530

    Recipe cart project started

--- recipe-cart/src/app/app.component.ts
+++ recipe-cart/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'app';
+  feature = 'recipes';
+
+  featureWasSelected(feature: string) {
+    this.feature = feature;
+  }
 }
